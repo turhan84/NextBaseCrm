60cdeca687d47464dd3f494151c918b5e69396df
Christopher <148374799+Christopher-To@users.noreply.github.com>
2024-03-20 17:01:09 +0000

    Work file is created.

---
+++ src/test/java/com/NextBaseCrm/pages/AppreciationPage.java
@@ -0,0 +1,35 @@
+package com.NextBaseCrm.pages;
+
+import com.NextBaseCrm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AppreciationPage {
+    public AppreciationPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy()
+    public WebElement a;
+
+    @FindBy()
+    public WebElement a;
+
+    @FindBy()
+    public WebElement a;
+
+
+    @FindBy()
+    public WebElement a;
+
+
+
+
+
+
+
+
+
+
+}
